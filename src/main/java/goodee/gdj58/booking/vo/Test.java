e635bf6e547b2003e80c970b075abd04ef564c40
TaeheeMin <mix264@naver.com>
2023-03-03 12:29:15 +0900

    구디아카데미 58기 파이널프로젝트 예약-기업 초기화

---
+++ src/main/java/goodee/gdj58/booking/vo/Test.java
@@ -0,0 +1,5 @@
+package goodee.gdj58.booking.vo;
+
+public class Test {
+
+}
---
+++ src/test/java/goodee/gdj58/booking/ApplicationTests.java
@@ -0,0 +1,13 @@
+package goodee.gdj58.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
